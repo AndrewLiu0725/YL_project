f1710ebed25ad6b7f5e2322f7f7d3611ab960ca8
AndrewLiu0725 <e5n41kb9n@gmail.com>
2021-03-25 20:38:05 +0800

    modify some details

--- RelaxtionTime.py
+++ RelaxtionTime.py
@@ -1,6 +1,6 @@
 # ===============================================================================
 # Copyright 2021 An-Jun Liu
-# Last Modified Date: 03/23/2021
+# Last Modified Date: 03/25/2021
 # ===============================================================================
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,23 +12,18 @@ import random
 from statsmodels.tsa.stattools import adfuller
 
 """
-This code is to calculate the relaxation time for twoc-ell and suspension system
+This code is to calculate the relaxation time for two-cell and suspension system
 """
 
 DEBUG = 0
 
 C_0 = 0 # global C(0)
 
-def C_tau_twoVar(x, a, b):
-    # a = C(0), b = t_relax
-    return a*np.exp(-x/b)
-
 def C_tau_oneVar(x, a):
     # a = t_relax
-    #if DEBUG: print("In C(tau), C_0 = ", C_0)
     return C_0*np.exp(-x/a)
 
-def calcRelaxationTime(phi, Ca, significance_level, maxlag_p, plot, avg_df_dict):
+def calcRelaxationTime(df, significance_level, maxlag_p, plot):
     """
     Input:
 
@@ -42,9 +37,7 @@ def calcRelaxationTime(phi, Ca, significance_level, maxlag_p, plot, avg_df_dict)
 
     [Is steady state?, t_relax, standard deviation errors on the parameters, ADF_usedlags]
     """
-
-    df = avg_df_dict[phi][Ca]
-
+    
     ### calculate stationarity
     starting_point = [0, 0.25, 0.5]
     used_lags = []
@@ -65,7 +58,7 @@ def calcRelaxationTime(phi, Ca, significance_level, maxlag_p, plot, avg_df_dict)
                 if DEBUG: print("p value =", adf_result[1])
         
         except Exception as e:
-            print("Error: Case (phi = {}, Ca = {}): \n{}".format(phi, Ca, e))
+            print(e)
             fail = 1
 
 
@@ -74,7 +67,6 @@ def calcRelaxationTime(phi, Ca, significance_level, maxlag_p, plot, avg_df_dict)
     steady_f = np.mean(df[-int(0.25*len(df)):]) # the avg of the last quarter time series
     deviation_f = df - steady_f
     T = len(df)
-    if DEBUG: print("T = ", T)
     tau_range = np.array([i for i in range(0, T)])
     C = []
 
@@ -86,7 +78,7 @@ def calcRelaxationTime(phi, Ca, significance_level, maxlag_p, plot, avg_df_dict)
     # fitting
     t_relax, rmse = [], []
     C = np.array(C)
-    global C_0
+    global C_0 # mark that the C_0 is a global variable (must)
     C_0 = C[0]
     
     end_list = [T, int(T/2), int(T/4)]
@@ -106,8 +98,7 @@ def calcRelaxationTime(phi, Ca, significance_level, maxlag_p, plot, avg_df_dict)
 
 
     for i in range(3):
-        popt, pcov = curve_fit(C_tau_oneVar, tau_range[:end_list[i]], C[:end_list[i]])
-        #perr = np.sqrt(np.diag(pcov))
+        popt = curve_fit(C_tau_oneVar, tau_range[:end_list[i]], C[:end_list[i]])[0]
 
         if plot:
             ax2.plot(tau_range, C_tau_oneVar(tau_range, *popt), color_list[i], label = text_list[i]+"\nt_relax={}".format(int(popt[0])))
@@ -130,50 +121,41 @@ if __name__ == "__main__":
     start_time = time.time()
 
     # read the data
-    system = "TwoCell"
-    with open("Data/AverageDF_{}.pickle".format(system), 'rb') as handle:
+    system_list = ["TwoCell", "Suspension"]
+    time_series_type_list = ["Indivisual", "EnsembleAveraged"]
+    system, time_series_type = system_list[0], time_series_type_list[1]
+    print("Current task: {}, {}\n".format(system, time_series_type))
+
+    with open("Data/AverageDF_{}_{}.pickle".format(system, time_series_type), 'rb') as handle:
         avg_df_dict = pickle.load(handle)
 
-    TEST = 1
+    TEST = 0
 
     if TEST:
-        phi = list(avg_df_dict.keys())[0]
-        Ca = list(avg_df_dict[phi].keys())[0]
-        print(calcRelaxationTime(phi, Ca, 0.05, 50, 0, avg_df_dict))
-        #print(calcRelaxationTime(4.9488, 0.08, 0.05, 100, 1, avg_df_dict))
-        #print(calcRelaxationTime(4.3, 0.13, 0.05, 1))
-        #print(calcRelaxationTime(4.0, 0.1, 0.05, 1))
-        
-        """
         for _ in range(5):
             phi = random.choice(list(avg_df_dict.keys()))
             Ca = random.choice(list(avg_df_dict[phi].keys()))
-            print(calcRelaxationTime(phi, Ca, 0.05, 1))
-        """
-        
-        
+            calcRelaxationTime(avg_df_dict[phi][Ca], 0.05, 50, 1)
 
     else:
         relaxation_time = []
         total_count = 0
-        autoregressive_order_p = 50
-
-        p_used = []
+        autoregressive_order_p = 30
 
         for phi in avg_df_dict.keys():
             for Ca in avg_df_dict[phi].keys():
-                total_count += 1
-                result = calcRelaxationTime(phi, Ca, 0.05, autoregressive_order_p, 0, avg_df_dict)
-                if result[0]:
-                    relaxation_time.append(np.mean(result[1]))
-                    p_used.append(np.mean(result[3]))
+                for df in avg_df_dict[phi][Ca]:
+                    total_count += 1
+                    result = calcRelaxationTime(df, 0.05, autoregressive_order_p, 0)
+                    if result[0]:
+                        relaxation_time.append(np.mean(result[1]))
 
         plt.hist(relaxation_time)
         plt.xlabel("Relaxation time")
         plt.ylabel("Count")
-        plt.title("Relaxation time ({}, maxlag = {}, {}%)".format(system, autoregressive_order_p, round(len(relaxation_time)/total_count, 2)*100))
+        plt.title("Relaxation time ({}, {}, maxlag = {}, {}%)".format(system, time_series_type, autoregressive_order_p, round(len(relaxation_time)/total_count, 2)*100))
         #plt.show()
-        plt.savefig("./Pictures/{}System_RelaxationTime_p_{}_Histogram.png".format(system, autoregressive_order_p), dpi = 200)
+        plt.savefig("./Pictures/{}System_RelaxationTime_{}_p_{}_Histogram.png".format(system, time_series_type, autoregressive_order_p), dpi = 200)
         plt.close()
 
     print('\nTotal time elapsed = {}'.format(str(datetime.timedelta(seconds=time.time()-start_time))))
