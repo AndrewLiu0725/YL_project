b06284d0ef66c4d5b3cc45ae49beccacf5a2e824
AndrewLiu0725 <e5n41kb9n@gmail.com>
2021-04-01 16:58:54 +0800

    finalize the definition of stationarity

--- RelaxtionTime.py
+++ RelaxtionTime.py
@@ -1,6 +1,6 @@
 # ===============================================================================
 # Copyright 2021 An-Jun Liu
-# Last Modified Date: 03/29/2021
+# Last Modified Date: 04/01/2021
 # ===============================================================================
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,6 +9,7 @@ import time
 import datetime
 import pickle
 from statsmodels.tsa.stattools import adfuller
+import random
 
 """
 This code is to calculate the relaxation time for two-cell and suspension system
@@ -22,9 +23,11 @@ def C_tau_oneVar(x, a):
     # a = t_relax
     return C_0*np.exp(-x/a)
 
-def calcRelaxationTime(df, significance_level, maxlag_p, plot):
+def calcRelaxationTime(ts, significance_level, maxlag_p, test_length, plot):
     """
     Input:
+
+    ts: 1-D numpy array
 
     significance_level: alpha value in null hypothesis test, recommeded value: 0.05
 
@@ -32,51 +35,46 @@ def calcRelaxationTime(df, significance_level, maxlag_p, plot):
     The adfuller function will automatically determining the lag length among the values [0, maxlag].
     The default method is that the number of lags is chosen to minimize the corresponding information criterion
 
+    test_length: ts[-test_length:] will be tested if it is stationary. recommended value: 1000
+
     Output:
 
-    [Is steady state?, t_relax, standard deviation errors on the parameters, ADF_usedlags]
+    [Is steady state?, t_relax, standard deviation errors on the parameters]
     """
     
     ### calculate stationarity
-    starting_point = [0, 0.25, 0.5]
-    used_lags = []
+    T = len(ts)
     unstable = 0
-    fail = 0
+    fail = 1
 
-    # iterate through 3 different sections of time series
-    for i in range(3):
+    if T > test_length:
         try:
-            adf_result = adfuller(df[int(starting_point[i]*len(df)):], maxlag = maxlag_p)
-
+            adf_result = adfuller(ts[-test_length:], maxlag = maxlag_p)
+            #print(adf_result[1])
             # p value > alpha, fail to reject the null hypothesis (non-stationary) means the time series is not stationary
             if adf_result[1] > significance_level:
                 unstable = 1
-                break
+            # p value <= alpha, the last test_length time series is stationary according to the adf test
             else:
-                used_lags.append(adf_result[2])
-                if DEBUG: print("p value =", adf_result[1])
+                fail = 0
         
         except Exception as e:
             print(e)
-            fail = 1
 
 
     ### calculate the relxation time
     # setup
-    steady_f = np.mean(df[-int(0.25*len(df)):]) # the avg of the last quarter time series
-    deviation_f = df - steady_f
-    T = len(df)
-    tau_range = np.array([i for i in range(0, T)])
-    C = []
+    steady_ts = np.mean(ts[-test_length:]) # the avg of the test_length time series
+    deviation_ts = ts - steady_ts
+    tau_range = np.arange(0, T)
+    C = np.zeros(T)
 
     # calculate C(tau)
     for tau in tau_range:
-        C_tmp = np.dot(deviation_f[:T-tau], deviation_f[tau:])/(T-tau)
-        C.append(C_tmp)
+        C[tau] = np.dot(deviation_ts[:T-tau], deviation_ts[tau:])/(T-tau)
 
     # fitting
     t_relax, rmse = [], []
-    C = np.array(C)
     global C_0 # mark that the C_0 is a global variable (must)
     C_0 = C[0]
     
@@ -86,8 +84,8 @@ def calcRelaxationTime(df, significance_level, maxlag_p, plot):
 
     if plot:
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (9, 6))
-        fig.suptitle("Relaxation time ({}): phi = {}, Ca = {}".format(system, phi, Ca), fontsize = 20)
-        ax1.plot(list(range(T)), df)
+        fig.suptitle("Relaxation Time Analysis", fontsize = 20)
+        ax1.plot(tau_range, ts)
         ax1.set_title("Doublet fraction time series", fontsize = 15)
         ax1.set(xlabel = "time", ylabel = "doublet fraction")
 
@@ -112,7 +110,7 @@ def calcRelaxationTime(df, significance_level, maxlag_p, plot):
         plt.subplots_adjust(top = 0.85)
         plt.show()
 
-    return [False] if (unstable or fail) else [True, t_relax, rmse, used_lags]
+    return [False] if (unstable or fail) else [True, t_relax, rmse]
 
 
 def analyzeRelaxationTime(variable_type, system, time_series_type, plot, save):
@@ -142,7 +140,7 @@ def analyzeRelaxationTime(variable_type, system, time_series_type, plot, save):
             for ts in ts_dict[phi][Ca]:
                 total_count += 1
                 ensemble_count += 1
-                result = calcRelaxationTime(ts, 0.05, autoregressive_order_p, 0)
+                result = calcRelaxationTime(ts, 0.05, autoregressive_order_p, 1000, 0)
                 if result[0]:
                     relaxation_time.append(np.mean(result[1]))
                 else:
@@ -164,7 +162,17 @@ def analyzeRelaxationTime(variable_type, system, time_series_type, plot, save):
     if save:
         with open("UnstationaryRatio_{}_{}_{}.pickle".format(variable_type, system, time_series_type), 'wb') as handle:
             pickle.dump(unstationary_ratio, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    
+
+
+def test(variable_type, system, time_series_type, random_flag, phi, Ca):   
+    with open("./Data/{}_{}_{}.pickle".format(variable_type, system, time_series_type), 'rb') as handle:
+        ts_dict = pickle.load(handle)
+    autoregressive_order_p = 50
+    if random_flag:
+        phi = random.choice(list(ts_dict.keys()))
+        Ca = random.choice(list(ts_dict[phi].keys()))
+    print("phi = {}, Ca = {}".format(phi, Ca))
+    print(calcRelaxationTime(ts_dict[phi][Ca][0][1:], 0.05, autoregressive_order_p, 1000, 1))
 
 
 if __name__ == "__main__":
@@ -176,6 +184,6 @@ if __name__ == "__main__":
     for system in system_list:
         for time_series_type in time_series_type_list:
             print("Current task: {}, {}\n".format(system, time_series_type))
-            analyzeRelaxationTime("IV", system, time_series_type, 0, 1)
+            analyzeRelaxationTime("IV", system, time_series_type, 1, 1)
 
     print('\nTotal time elapsed = {}'.format(str(datetime.timedelta(seconds=time.time()-start_time))))
